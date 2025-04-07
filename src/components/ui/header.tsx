d6fc6481973cca0d70b1b9d720335f5e23d9e449
Gustavo Dantas <gusdantas15@gmail.com>
2025-04-07 10:03:44 -0300

    chore(header): changes header background to accent color

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,9 +1,10 @@
-import logo from "@/assets/logo.png"
+import logo from '@/assets/logo.png';
 
 export function Header() {
-  const altText = "Flat vector logo showing Earth in green and blue tones, surrounded by travel-related icons including a red passport, a vintage suitcase, and a location pin, symbolizing global adventure and trip planning";
+  const altText =
+    'Flat vector logo showing Earth in green and blue tones, surrounded by travel-related icons including a red passport, a vintage suitcase, and a location pin, symbolizing global adventure and trip planning';
   return (
-    <header className="bg-sky-500 p-4 flex items-center">
+    <header className="bg-accent p-4 flex items-center">
       <img src={logo} alt={altText} className="h-14 w-auto" />
     </header>
   );
